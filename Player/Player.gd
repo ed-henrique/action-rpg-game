9cc9c6304c03f655e42578c032099df4e363fc61
ed-henrique <edu.hen.fm@gmail.com>
2022-03-24 14:50:02 -0400

    changing how movement works

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,17 +1,17 @@
 extends KinematicBody2D
 
+# const FRICTION = 10
+const ACC_GLOBAL = 10
+const MAX_SPEED = 100
+
 var velocity = Vector2.ZERO
-var speed = 100
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
 	
-	if input_vector != Vector2.ZERO:
-		velocity = input_vector
-	else:
-		velocity = Vector2.ZERO
+	velocity = velocity.move_toward(input_vector * MAX_SPEED, ACC_GLOBAL)
 	
-	print(velocity)
-	move_and_collide(velocity * speed * delta)
+	move_and_collide(velocity * delta)
